20ee2ce6d3f12ededbdec3b55496e452eede74b2
latifa <latifawakili3@gmail.com>
2025-01-18 20:37:02 -0800

    added all neded files

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center bg-blue-500 text-white">
+      <h1 className="text-4xl font-bold">Welcome to Next.js with TailwindCSS!</h1>
+    </div>
+  );
+}
